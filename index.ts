24c7324ec978e5fc303dfc59ddd9a11470717d45
Ayesha1130 <ayeshaiqbal771@gmail.com>
2024-04-18 13:11:57 +0500

    add new comments

--- index.ts
+++ index.ts
@@ -1,11 +1,16 @@
 #! /usr/bin/env node
+
+//Importing Inquirer
 import inquirer from "inquirer";
 
+//Importing Chalk
 import chalk from "chalk";
 
+//Printing Greetings Text with chalk
 console.log(chalk.bgMagenta("\n **** Welcome To WOrds Counter CLI base Project **** ")
 );
 
+//Declare a Const "Answer"
 const answers: {
   sentence: string;
 } = await inquirer.prompt([
@@ -18,8 +23,11 @@ const answers: {
 
 const words = answers.sentence.trim().split(" ");
 
+//Printing the array of words and applying chalk
 console.log(chalk.bgGreen(`\n ${words}`));
 
+ //Printing the words count with chalk
 console.log(chalk.blue.bold(`\n Your Sentence Words Counter Is: ${words.length}`));
 
+//Printing Greetings Message with chalk
 console.log(chalk.bgYellow("\n **** Thank you Using Words Counter **** "));
